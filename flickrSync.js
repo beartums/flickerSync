edf9ee2cfcc9f1aa757d5c0c732c55dfda2a23fb
beartums <eric@griffithnet.com>
2014-08-10 14:13:20 -0400

    Remove promises; don't worry about Exif for the moment

--- flickrSync.js
+++ flickrSync.js
@@ -1,4 +1,5 @@
 var config = module.parent.exports.config;
+//console.log(config);
 var fs 			= require('fs');
 var nodeIni 		= require('node-ini');
 var xml2js 	= require('xml2js');
@@ -12,7 +13,7 @@ var Q = require('q')
     path = path || '';
     parent = parent || null;
     var fullPath = makePath(directory,path);
-
+    //console.log(fullPath);
     var statObj = fs.statSync(fullPath);
     var fsObj = {};
     fsObj.fullPath = fullPath;
@@ -22,27 +23,38 @@ var Q = require('q')
     fsObj.path = path;
     fsObj.depth = depth;
     fsObj.parentId = parent && parent.Id ? parent.Id : null;
+    if (fsObj.type === 'file') {
+      if (!isExtIncluded(fsObj.name)) return null;
+      fsObj.ext = getExtension(fsObj.name);
+      return fsObj;
+    }
 
-    if (fsObj.type==='file' && !extIsIncluded(fsObj.name,config.includedExts)) {
-      return null;
-    }
-    if (fsObj.type==='file') {
-      fsObj = getFile(fsObj,parent);
-    }
+    fsObj.files = {};
 
     if (fsObj.type==='dir') {
-      fsObj.children = [];
+      fsObj.children=[];
       fsObj.iniFile = getIniFile(fullPath);
       console.log(fullPath);
       var children = fs.readdirSync(fullPath);
-      //var count = 5
+      // Directories go into .children array.  Files get summarized in .files
       for (var i = 0; i < children.length; i++) {
-        var child = explodeDir(children[i],fsObj,fullPath,depth+1);
-        if (child) fsObj.children.push(child);
-        //if (child.type==='file') break;
+        var child = exports.explodeDir(children[i],fsObj,fullPath,depth+1);
+        if (child && child.type === 'dir') {
+          fsObj.children.push(child);
+          //console.log(child.files);
+        } else if (child && child.type === 'file') {
+          child.ext = child.ext && child.ext != '' ? child.ext : 'unknown';
+          if (fsObj.files[child.ext]) {
+            fsObj.files[child.ext] += 1;
+          } else {
+            fsObj.files[child.ext] = 1;
+          }
+        }
       }
+
     }
 
+    fsObj.scanDate = new Date();
     return fsObj;
   }
 
@@ -60,6 +72,7 @@ function getFile(file,parent) {
   file.keywords = iniObj.keywords ? iniObj.keywords : '';
   file.albums = [];
   file.flickrAlbum = '';
+  file.parentId = parent.id;
   var iniFaces = [];
   if (iniObj.faces) {
     var faces = iniObj.faces.split(';');
@@ -81,8 +94,12 @@ function getFile(file,parent) {
       iniFaces.push(faceObj);
     }
     file.faces = iniFaces;
-    exifTool.parsePromise(file.fullPath, ['-j','-g','-u'])
-      .then(saveFsobjPromise.bind(file));
+    return exifTool.parsePromise(file.fullPath, ['-j','-g','-u'])
+      .then(function (exifdata) {
+        this.exif = JSON.parse(exifdata)[0];
+        return saveFsobjPromise.bind(this)();
+        }.bind(file)
+      );
   }
 }
 
@@ -90,9 +107,10 @@ function getExifPromise(file) {
   return exifTool.parsePromise(file.fullPath, ['-j','-g','-u']);
 }
 // this function must be bound to the object being saved
-function saveFsobjPromise(exifdata) {
-  this.exif = JSON.parse(exifdata)[0];
-  var fileString = JSON.stringify(this);
+
+function saveFsobjPromise() {
+  //console.log(this);
+  var saveString = JSON.stringify(this);
   var options = {
     host: config.db.hostname,
     path: config.db.path,
@@ -100,7 +118,7 @@ function saveFsobjPromise(exifdata) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Content-Length': fileString.length
+      'Content-Length': saveString.length
     }
   };
 
@@ -111,10 +129,11 @@ function saveFsobjPromise(exifdata) {
   req.on('error', function(err) {
     deferred.reject(err);
   })
-  req.write(fileString);
+  req.write(saveString);
   req.end();
   return deferred.promise;
 }
+
 function makePath(directory,parentPath) {
   parentPath = parentPath || '';
   if (parentPath == '') return directory;
@@ -122,6 +141,7 @@ function makePath(directory,parentPath) {
   var path = parentPath + '/' + directory;
   return path;
 }
+
 function getIniFile(path) {
   var ini = null, iniFileNames = config.iniFiles;
 
@@ -138,10 +158,17 @@ function getIniFile(path) {
   }
   return ini;
 }
-function extIsIncluded(fileName, includedExts) {
+
+function isExtIncluded(fileName, includedExts) {
   includedExts = includedExts || config.includedExts;
-  extRE = new RegExp('\.(' + includedExts.join('|') + ')$','i');
+  var extRE = new RegExp('\.(' + includedExts.join('|') + ')$','i');
   return extRE.test(fileName);
+}
+
+function getExtension(fileName) {
+  var extRE = /\.([^\.]{1,10}$)/;
+  var matches = extRE.exec(fileName);
+  return (!matches || !matches[1]) ? '' : matches[1].toUpperCase();
 }
 
 function saveDirPromise(dirObj) {
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var http 		= require('http');
-var flickrSync = require('./flickrSync.js');
-
 var app     = express();
+var fs = require('fs');
+
+
 // var mongo = require("mongodb");
 
 var config = {
@@ -10,11 +10,11 @@ var config = {
 			hostname: 'localhost',
 			port: 3000,
 			path: '/photos/directory'
-	}
-	rootDirectory: 'N:\\Shared\\Photos\\2009-12-31 - Happy New Year\\IMG_0002.JPG',
+	},
+	rootDirectory: 'N:\\Shared\\Photos',
 	iniFiles: ['.picasa.ini','Picasa.ini'],
 	originalsFolder: '.picasaoriginals',
-	includedExts: ['JPG','GIF','PNG','TIF','AVI','MP4','MPG'],
+	includedExts: ['JPG', 'GIF', 'PNG', 'TIF', 'AVI', 'MP4', 'MPG'],
 	picasaContacts: 'C:\\Users\\Eric\\AppData\\Local\\Google\\Picasa2\\contacts\\contacts.xml',
 	exifToolOutput: '\\\\192.168.0.253\\GrifNas\\Shared\\web\\flickerSync\\vendor\\exiftool\\outfile.json',
 	// folders that should be collections rather than albums
@@ -25,29 +25,30 @@ var config = {
 }
 module.exports.config = config;
 
+var http 		= require('http');
+var flickrSync = require('./flickrSync.js');
+
 var headers = {
 	'Content-Type': 'application/json',
 	'Content-Length': 0
 };
+
 var options = {
-	host: 'localhost',
-	port: 3000,
-	path: '/photos/directory',
+	host: config.db.hostname,
+	path: config.db.path,
+	port: config.db.port,
 	method: 'POST',
 	headers: headers
-}
-var req = http.request(options,function(res){
-	console.log(res);
-});
-req.on('error', function(err) {
-	console.log(err);
-})
+};
+
 
 var dirTree = flickrSync.explodeDir(config.rootDirectory);
-console.log(dirTree);
+ console.log(dirTree);
 var dirTreeString = JSON.stringify(dirTree);
-options.headers['Content-Length'] = dirTreeString.length;
 var file = fs.writeFileSync(__dirname + '/outfile',dirTreeString);
+
+options.headers['Content-Length'] = dirTreeString.length;
+var req = http.request(options)
 req.write(dirTreeString);
 req.end
 
@@ -55,7 +56,6 @@ req.end
 
 */
 // })
-//
-// app.listen('3005')
-// console.log('Magic happens on port 3005');
+//app.listen('3005')
+//console.log('Magic happens on port 3005');
 // exports = module.exports = app;
